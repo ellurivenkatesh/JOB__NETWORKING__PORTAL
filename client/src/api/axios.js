a3a649fecc0b4e8e8296fdfebfd6c826187e661e
unknown <ellurivenkatesh2005@gmail.com>
2025-08-05 02:23:41 +0530

    Final final commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-networking-portal-rwhp.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
